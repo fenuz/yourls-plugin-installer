afa03eed82db9f4a132dcb66c0819d068ae8fc9e
Frank Matheron <frankmatheron@gmail.com>
2013-06-10 13:37:51 +0200

    initial commit

---
+++ lib/Kennisnet/Yourls/Composer/PluginInstaller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kennisnet\Yourls\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class PluginInstaller extends LibraryInstaller
+{
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $fullname = explode('/', $package->getPrettyName());
+        $pluginName = $fullname[1];
+        
+        $yourlsPluginDir = 'yourls/user/plugins';
+        if ($this->composer->getPackage()) {
+            $extra = $this->composer->getPackage()->getExtra();
+            if (!empty($extra['yourls-plugin-dir'])) {
+                $yourlsPluginDir = $extra['yourls-plugin-dir'];
+            }
+        }        
+        
+        return $yourlsPluginDir . '/' . $pluginName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'yourls-plugin' === $packageType;
+    }
+
+}
